469b29b80c45048c006356120de0770f646e35a8
Ross Shtivelberg <RossTheDev@gmail.com>
2018-07-12 19:47:02 -0400

    mobile first css

--- omk/src/App.js
+++ omk/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {FaBars} from 'react-icons/lib/fa'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-logo">logo</h1>
+          <h1 className="App-menu"> <FaBars /> </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <p className="App-intro">
+            {"welcome"}
+          </p>
+          <p className="App-intro2">
+            {"we specialize in web & logo design"}
+          </p>
+
+          <div className="card-container">
+          </div>
+        </main>
+
       </div>
     );
   }
