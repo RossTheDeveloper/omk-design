34e86c7e39bf7e383eac6691c791aa7e13707cdf
Ross Shtivelberg <RossTheDev@gmail.com>
2018-07-12 23:56:29 -0400

    globe animation added

--- omk/src/App.js
+++ omk/src/App.js
@@ -8,18 +8,23 @@ class App extends Component {
     return (
       <div className="App">
 
+        <div className="Header-fix">
         <header className="App-header">
           <h1 className="App-logo">logo</h1>
           <h1 className="App-menu"> <FaBars /> </h1>
         </header>
+        </div>
 
         <main>
+
+          <div className="intro-container">
           <p className="App-intro">
             {"Welcome"}
           </p>
           <p className="App-intro2">
             {"Looking for Website Design?"}
           </p>
+          </div>
 
           <div className="quote-container">
           <div className="quote-icon">
